b3f6b84be2303c14bce7b5f3ea96118fd350091b
Velizar Velizarov <vlvsv87@gmail.com>
2015-10-19 12:56:29 +0300

    Implemented Update and Delete actions in UsersApiController

--- OneBitTask.Web/Controllers/UsersApiController.cs
+++ OneBitTask.Web/Controllers/UsersApiController.cs
@@ -1,8 +1,48 @@
 ﻿namespace OneBitTask.Web.Controllers
 {
+    using System;
     using System.Web.Http;
+
+    using Data.Repositories;
+    using Models;
 
     public class UsersApiController : ApiController
     {
+        private UsersRepository data = new UsersRepository();
+
+        //TODO: Validation
+        [HttpPut]
+        public IHttpActionResult ChangeStatus(string id)
+        {
+            var idAsGuid = new Guid(id);
+            var user = this.data.GetById(idAsGuid);
+            if (user == null)
+            {
+                return BadRequest(String.Format("User with Id: {0} does not exist.", id));
+            }
+            if (user.Status == StatusType.Active)
+            {
+                user.Status = StatusType.Inactive;
+            }
+            else
+            {
+                user.Status = StatusType.Active;
+            }
+
+            this.data.Update(user);
+            this.data.SaveChanges();
+
+            return Ok(user.Status);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(string id)
+        {
+            var idAsGuid = new Guid(id);
+            this.data.Delete(idAsGuid);
+            this.data.SaveChanges();
+
+            return Ok();
+        }
     }
 }
