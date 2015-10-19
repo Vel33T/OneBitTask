df60e6eee0d144273e025b062f44d0298fcf9835
Velizar Velizarov <vlvsv87@gmail.com>
2015-10-19 09:10:58 +0300

    Added UsersDbContext with corresponding interface

---
+++ OneBitTask.Data/IUsersDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace OneBitTask.Data
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    using Models;
+
+    public interface IUsersDbContext
+    {
+        IDbSet<User> Users { get; set; }
+
+        void SaveChanges();
+
+        IDbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+    }
+}
---
+++ OneBitTask.Data/UsersDbContext.cs
@@ -0,0 +1,27 @@
+﻿namespace OneBitTask.Data
+{
+    using Models;
+    using System.Data.Entity;
+    using System;
+
+    public class UsersDbContext : DbContext, IUsersDbContext
+    {
+        public UsersDbContext()
+            : base("UsersDatabase")
+        {
+
+        }
+
+        public virtual IDbSet<User> Users { get; set; }
+
+        public new void SaveChanges()
+        {
+            base.SaveChanges();
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
