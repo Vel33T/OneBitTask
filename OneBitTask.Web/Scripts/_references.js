f3736fb16f7424440d0e0cf13475207338cce9f1
Velizar Velizarov <vlvsv87@gmail.com>
2015-10-19 18:24:55 +0300

    Added custom JavaScript file to Scripts with the needed logic Minor changes on Index view

--- OneBitTask.Web/Scripts/_references.js
+++ OneBitTask.Web/Scripts/_references.js
@@ -1,3 +1,4 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="bootstrap.js" />
+/// <reference path="custom.js" />
 /// <reference path="jquery-1.9.1.js" />
---
+++ OneBitTask.Web/Scripts/custom.js
@@ -0,0 +1,31 @@
+﻿$('.btnChangeStatus').on('click', function () {
+    var self = this;
+    var id = $(this).val();
+    var url = '/Api/UsersApi/ChangeStatus' + '?id=' + id;
+
+    var request = $.ajax({
+        url: url,
+        type: "PUT",
+        contentType: 'application/json',
+        success: function (newStatus) {
+            $(self).parent().find('span').text('Status: ' + newStatus);
+        }
+    });
+
+});
+
+$('.btnDelete').on('click', function () {
+    var self = this;
+    var id = $(this).val();
+    var url = '/Api/UsersApi/DeleteUser' + '?id=' + id;
+
+    var request = $.ajax({
+        url: url,
+        type: "DELETE",
+        contentType: 'application/json',
+        success: function () {
+            $(self).parent().parent().fadeOut(1000);
+        }
+    });
+
+});
