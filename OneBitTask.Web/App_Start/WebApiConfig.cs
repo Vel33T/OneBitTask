48eed22b49c67e32d207fc73cae2d453d8d3c79b
Velizar Velizarov <vlvsv87@gmail.com>
2015-10-19 10:08:24 +0300

    Modifying WebApiConfig by adding {action} to the routeTemplate for more control

--- OneBitTask.Web/App_Start/WebApiConfig.cs
+++ OneBitTask.Web/App_Start/WebApiConfig.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-
-namespace OneBitTask.Web
+﻿namespace OneBitTask.Web
 {
+    using System.Web.Http;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -16,7 +13,7 @@ namespace OneBitTask.Web
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
