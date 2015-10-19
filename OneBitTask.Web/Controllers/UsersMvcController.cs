9719f41e4f254e87cbc1de00b0f822d789bc9250
Velizar Velizarov <vlvsv87@gmail.com>
2015-10-19 10:44:12 +0300

    Implemented Index action in UsersMvcController Added Index view

--- OneBitTask.Web/Controllers/UsersMvcController.cs
+++ OneBitTask.Web/Controllers/UsersMvcController.cs
@@ -1,13 +1,20 @@
 ﻿namespace OneBitTask.Web.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
+
+    using Data.Repositories;
+    using ViewModels;
 
     public class UsersMvcController : Controller
     {
-        // GET: UsersMvc
+        private UsersRepository data = new UsersRepository();
+
         public ActionResult Index()
         {
-            return View();
+            var allUsers = this.data.All().Select(UserViewModel.FromUser).ToList();
+
+            return View(allUsers);
         }
     }
 }
